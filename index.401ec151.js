00174083d6f121b906335d1ff88ad3f1fa49a879
an1skat <156462349+an1skat@users.noreply.github.com>
2024-06-02 14:15:16 +0000

    Deploying to gh-pages from @ an1skat/Simply-Chocolate@aa5ebf176e2eb21d1584f097e0fe227a01e2baf4 🚀

---
+++ index.401ec151.js
@@ -0,0 +1,2 @@
+new Swiper(".swiper",{pagination:{el:".swiper-pagination",type:"bullets",clickable:!0},freeMode:!0});
+//# sourceMappingURL=index.401ec151.js.map
---
+++ index.7995af55.js
@@ -0,0 +1,2 @@
+new Swiper(".swiper",{pagination:{el:".swiper-pagination",type:"bullets",clickable:!0},freeMode:!0});
+//# sourceMappingURL=index.7995af55.js.map
