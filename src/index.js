f915cf042d63f42f7ada31e4841721176d36e527
an1skat <156462349+an1skat@users.noreply.github.com>
2024-06-04 12:57:51 +0300

    finish styles for products sect

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
-const mySwiper = new Swiper('.swiper', {
-  pagination: {
-    el: '.swiper-pagination',
-    type: 'bullets',
-    clickable: true,
-  },
-  freeMode: true,
+document.addEventListener("DOMContentLoaded", function() {
+  const swiper = new Swiper('.swiper', {
+    loop: true,
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true,
+    },
+    spaceBetween: 18,
+    breakpoints: {
+      768: {
+        slidesPerView: 3
+      },
+      1200: {
+        slidesPerView: 4
+      }
+    }
+  });
 });
