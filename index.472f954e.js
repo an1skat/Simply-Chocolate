4a797e8b1390367a87832dc3c03d0f4587083c38
an1skat <156462349+an1skat@users.noreply.github.com>
2024-06-07 17:33:31 +0000

    Deploying to gh-pages from @ an1skat/Simply-Chocolate@9ceed370b158f39841e3523cc4e6439c19471b2f 🚀

---
+++ index.472f954e.js
@@ -0,0 +1,2 @@
+document.addEventListener("DOMContentLoaded",(function(){new Swiper(".products-swiper",{loop:!0,pagination:{el:".swiper-pagination",type:"bullets",clickable:!0},breakpoints:{768:{slidesPerView:3},1200:{slidesPerView:4}}}),new Swiper(".reviews-swiper",{loop:!0,pagination:{el:".swiper-pagination",type:"bullets",clickable:!0},breakpoints:{768:{slidesPerView:2},1200:{slidesPerView:3}}})}));
+//# sourceMappingURL=index.472f954e.js.map
