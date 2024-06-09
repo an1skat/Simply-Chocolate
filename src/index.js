25bc21f7fb8639bf551f5f709b5b9405fac1f296
an1skat <156462349+an1skat@users.noreply.github.com>
2024-06-09 20:09:19 +0300

    finish second reviews modal

--- src/index.js
+++ src/index.js
@@ -37,20 +37,53 @@ document.addEventListener('DOMContentLoaded', function () {
 const refs = {
   openBtn: document.querySelector('[data-modal-open]'),
   closeBtn: document.querySelector('[data-modal-close]'),
+  closeBtn2: document.querySelector('[data-modal-close2]'),
   modal: document.querySelector('[data-modal]'),
+  modal2: document.querySelector('[data-modal2]'),
   overlay: document.querySelector('[data-modal-overlay]'),
+  sendBtn: document.querySelector('.reviews__btn--submit'),
 };
+
 function toggleModal() {
   refs.modal.classList.toggle('is-hidden');
   refs.overlay.classList.toggle('is-hidden');
   document.body.classList.toggle('no-scroll');
 }
+
+function openSecondModal() {
+  refs.modal.classList.add('is-hidden');
+  refs.modal2.classList.remove('is-hidden');
+}
+
+function closeSecondModal() {
+  refs.modal2.classList.add('is-hidden');
+  refs.overlay.classList.add('is-hidden');
+  document.body.classList.remove('no-scroll');
+}
+
 refs.openBtn.addEventListener('click', toggleModal);
 refs.closeBtn.addEventListener('click', toggleModal);
-refs.overlay.addEventListener('click', toggleModal);
+refs.overlay.addEventListener('click', () => {
+  if (!refs.modal.classList.contains('is-hidden')) {
+    toggleModal();
+  } else if (!refs.modal2.classList.contains('is-hidden')) {
+    closeSecondModal();
+  }
+});
+refs.closeBtn2.addEventListener('click', closeSecondModal);
+
+refs.sendBtn.addEventListener('click', function (e) {
+  e.preventDefault();
+  openSecondModal();
+});
+
 document.addEventListener('keydown', function (e) {
   if (e.key === 'Escape') {
-    toggleModal();
+    if (!refs.modal.classList.contains('is-hidden')) {
+      toggleModal();
+    } else if (!refs.modal2.classList.contains('is-hidden')) {
+      closeSecondModal();
+    }
   }
 });
 
